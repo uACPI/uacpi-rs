7ed21f0ded692eb1bf9da66a176f6de696646133
Shinribo <73356576+Shinribo@users.noreply.github.com>
2025-03-18 09:45:30 +0100

    Reworked UacpiStatus Code after short Code Review

--- uacpi/src/status.rs
+++ uacpi/src/status.rs
@@ -1,7 +1,73 @@
 
+#[repr(C)]
+pub(crate) struct Status(i32);
+
+impl Status {
+    
+    pub(crate) fn evaluate_uacpi_status(value: i32) -> Result<(), UacpiError> {
+        match value {
+            uacpi_sys::UACPI_STATUS_OK => Ok(()),
+            uacpi_sys::UACPI_STATUS_MAPPING_FAILED => Err(UacpiError::MappingFailed),
+            uacpi_sys::UACPI_STATUS_OUT_OF_MEMORY => Err(UacpiError::OutOfMemory),
+            uacpi_sys::UACPI_STATUS_BAD_CHECKSUM => Err(UacpiError::BadChecksum),
+            uacpi_sys::UACPI_STATUS_INVALID_SIGNATURE => Err(UacpiError::InvalidSignature),
+            uacpi_sys::UACPI_STATUS_INVALID_TABLE_LENGTH => Err(UacpiError::InvalidTableLength),
+            uacpi_sys::UACPI_STATUS_NOT_FOUND => Err(UacpiError::NotFound),
+            uacpi_sys::UACPI_STATUS_INVALID_ARGUMENT => Err(UacpiError::InvalidArgument),
+            uacpi_sys::UACPI_STATUS_UNIMPLEMENTED => Err(UacpiError::Unimplemented),
+            uacpi_sys::UACPI_STATUS_ALREADY_EXISTS => Err(UacpiError::AlreadyExists),
+            uacpi_sys::UACPI_STATUS_INTERNAL_ERROR => Err(UacpiError::InternalError),
+            uacpi_sys::UACPI_STATUS_TYPE_MISMATCH => Err(UacpiError::TypeMismatch),
+            uacpi_sys::UACPI_STATUS_INIT_LEVEL_MISMATCH => Err(UacpiError::InitLevelMismatch),
+            uacpi_sys::UACPI_STATUS_NAMESPACE_NODE_DANGLING => {
+                Err(UacpiError::NamespaceNodeDangling)
+            }
+            uacpi_sys::UACPI_STATUS_NO_HANDLER => Err(UacpiError::NoHandler),
+            uacpi_sys::UACPI_STATUS_NO_RESOURCE_END_TAG => Err(UacpiError::NoResourceEndTag),
+            uacpi_sys::UACPI_STATUS_COMPILED_OUT => Err(UacpiError::CompiledOut),
+            uacpi_sys::UACPI_STATUS_HARDWARE_TIMEOUT => Err(UacpiError::HardwareTimeout),
+            uacpi_sys::UACPI_STATUS_TIMEOUT => Err(UacpiError::Timeout),
+            uacpi_sys::UACPI_STATUS_OVERRIDDEN => Err(UacpiError::Overriden),
+            uacpi_sys::UACPI_STATUS_DENIED => Err(UacpiError::Denied),
+    
+            uacpi_sys::UACPI_STATUS_AML_UNDEFINED_REFERENCE => {
+                Err(UacpiError::AmlUndefindedReference)
+            }
+            uacpi_sys::UACPI_STATUS_AML_INVALID_NAMESTRING => Err(UacpiError::AmlInvalidNamestring),
+            uacpi_sys::UACPI_STATUS_AML_OBJECT_ALREADY_EXISTS => {
+                Err(UacpiError::AmlObjectAlreadyExists)
+            }
+            uacpi_sys::UACPI_STATUS_AML_INVALID_OPCODE => Err(UacpiError::AmlInvalidOpcode),
+            uacpi_sys::UACPI_STATUS_AML_INCOMPATIBLE_OBJECT_TYPE => {
+                Err(UacpiError::AmlIncompatibleObjectType)
+            }
+            uacpi_sys::UACPI_STATUS_AML_BAD_ENCODING => Err(UacpiError::AmlBadEncoding),
+            uacpi_sys::UACPI_STATUS_AML_OUT_OF_BOUNDS_INDEX => Err(UacpiError::AmlOutOfBondsIndex),
+            uacpi_sys::UACPI_STATUS_AML_SYNC_LEVEL_TOO_HIGH => Err(UacpiError::AmlSyncLevelTooHigh),
+            uacpi_sys::UACPI_STATUS_AML_INVALID_RESOURCE => Err(UacpiError::AmlInvalidResource),
+            uacpi_sys::UACPI_STATUS_AML_LOOP_TIMEOUT => Err(UacpiError::AmlLoopTimeout),
+            uacpi_sys::UACPI_STATUS_AML_CALL_STACK_DEPTH_LIMIT => {
+                Err(UacpiError::AmlCallStackDepthLimit)
+            }
+    
+            _ => unreachable!("Undefined Value returned by UACPI"),
+        }
+    }
+
+    pub(crate) fn to_status(value: Result<(),UacpiError>) -> Status {
+        match value {
+            Ok(()) => Self(0),
+            Err(error) => Self(error as i32),
+        }
+    }
+
+
+}
+
+
 #[repr(i32)]
-#[derive(Clone,Copy,PartialEq, Eq, PartialOrd, Ord)]
-pub enum UacpiError{
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum UacpiError {
     MappingFailed = uacpi_sys::UACPI_STATUS_MAPPING_FAILED,
     OutOfMemory = uacpi_sys::UACPI_STATUS_OUT_OF_MEMORY,
     BadChecksum = uacpi_sys::UACPI_STATUS_BAD_CHECKSUM,
@@ -38,47 +104,7 @@ pub enum UacpiError{
 
 impl UacpiError {
 
-    pub fn from_raw(value: i32) -> Result<(), UacpiError> {
-        match value {
-            uacpi_sys::UACPI_STATUS_OK => Ok(()),
-            uacpi_sys::UACPI_STATUS_MAPPING_FAILED => Err(UacpiError::MappingFailed),
-            uacpi_sys::UACPI_STATUS_OUT_OF_MEMORY => Err(UacpiError::OutOfMemory),
-            uacpi_sys::UACPI_STATUS_BAD_CHECKSUM => Err(UacpiError::BadChecksum),
-            uacpi_sys::UACPI_STATUS_INVALID_SIGNATURE => Err(UacpiError::InvalidSignature),
-            uacpi_sys::UACPI_STATUS_INVALID_TABLE_LENGTH => Err(UacpiError::InvalidTableLength),
-            uacpi_sys::UACPI_STATUS_NOT_FOUND => Err(UacpiError::NotFound),
-            uacpi_sys::UACPI_STATUS_INVALID_ARGUMENT => Err(UacpiError::InvalidArgument),
-            uacpi_sys::UACPI_STATUS_UNIMPLEMENTED => Err(UacpiError::Unimplemented),
-            uacpi_sys::UACPI_STATUS_ALREADY_EXISTS => Err(UacpiError::AlreadyExists),
-            uacpi_sys::UACPI_STATUS_INTERNAL_ERROR => Err(UacpiError::InternalError),
-            uacpi_sys::UACPI_STATUS_TYPE_MISMATCH => Err(UacpiError::TypeMismatch),
-            uacpi_sys::UACPI_STATUS_INIT_LEVEL_MISMATCH => Err(UacpiError::InitLevelMismatch),
-            uacpi_sys::UACPI_STATUS_NAMESPACE_NODE_DANGLING => Err(UacpiError::NamespaceNodeDangling),
-            uacpi_sys::UACPI_STATUS_NO_HANDLER => Err(UacpiError::NoHandler),
-            uacpi_sys::UACPI_STATUS_NO_RESOURCE_END_TAG => Err(UacpiError::NoResourceEndTag),
-            uacpi_sys::UACPI_STATUS_COMPILED_OUT => Err(UacpiError::CompiledOut),
-            uacpi_sys::UACPI_STATUS_HARDWARE_TIMEOUT => Err(UacpiError::HardwareTimeout),
-            uacpi_sys::UACPI_STATUS_TIMEOUT => Err(UacpiError::Timeout),
-            uacpi_sys::UACPI_STATUS_OVERRIDDEN => Err(UacpiError::Overriden),
-            uacpi_sys::UACPI_STATUS_DENIED => Err(UacpiError::Denied),
-
-            uacpi_sys::UACPI_STATUS_AML_UNDEFINED_REFERENCE => Err(UacpiError::AmlUndefindedReference),
-            uacpi_sys::UACPI_STATUS_AML_INVALID_NAMESTRING => Err(UacpiError::AmlInvalidNamestring),
-            uacpi_sys::UACPI_STATUS_AML_OBJECT_ALREADY_EXISTS => Err(UacpiError::AmlObjectAlreadyExists),
-            uacpi_sys::UACPI_STATUS_AML_INVALID_OPCODE => Err(UacpiError::AmlInvalidOpcode),
-            uacpi_sys::UACPI_STATUS_AML_INCOMPATIBLE_OBJECT_TYPE => Err(UacpiError::AmlIncompatibleObjectType),
-            uacpi_sys::UACPI_STATUS_AML_BAD_ENCODING => Err(UacpiError::AmlBadEncoding),
-            uacpi_sys::UACPI_STATUS_AML_OUT_OF_BOUNDS_INDEX => Err(UacpiError::AmlOutOfBondsIndex),
-            uacpi_sys::UACPI_STATUS_AML_SYNC_LEVEL_TOO_HIGH => Err(UacpiError::AmlSyncLevelTooHigh),
-            uacpi_sys::UACPI_STATUS_AML_INVALID_RESOURCE => Err(UacpiError::AmlInvalidResource),
-            uacpi_sys::UACPI_STATUS_AML_LOOP_TIMEOUT => Err(UacpiError::AmlLoopTimeout),
-            uacpi_sys::UACPI_STATUS_AML_CALL_STACK_DEPTH_LIMIT => Err(UacpiError::AmlCallStackDepthLimit),
-
-            _ => unreachable!("Undefined Value returned by UACPI (Undefined Behavior)")
-        }
-    }
-
-    pub fn to_string(&self) -> &str {
+    pub fn as_str(&self) -> &str {
         match self {
             UacpiError::MappingFailed => "failed to map memory",
             UacpiError::OutOfMemory => "out of memory",
@@ -107,10 +133,12 @@ impl UacpiError {
             UacpiError::AmlIncompatibleObjectType => "incompatible AML object type",
             UacpiError::AmlBadEncoding => "bad AML instruction encoding",
             UacpiError::AmlOutOfBondsIndex => "out of bounds AML index",
-            UacpiError::AmlSyncLevelTooHigh => "AML attempted to acquire a mutex with a lower sync level",
+            UacpiError::AmlSyncLevelTooHigh => {
+                "AML attempted to acquire a mutex with a lower sync level"
+            }
             UacpiError::AmlInvalidResource => "invalid resource template encoding or type",
             UacpiError::AmlLoopTimeout => "hanging AML while loop",
-            UacpiError::AmlCallStackDepthLimit => "reached maximum AML call stack depth"
+            UacpiError::AmlCallStackDepthLimit => "reached maximum AML call stack depth",
         }
     }
 }
